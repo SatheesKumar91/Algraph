071d0fbb40a15719a3b5bb60a9c8bfb7775137a5
LiberaLongo <liberalongo@gmail.com>
2019-01-25 15:26:50 +0100

    altri test di grafica, configurazione del pesciolino XD

--- src/graph/GraficaTest.java
+++ src/graph/GraficaTest.java
@@ -15,14 +15,11 @@ public class GraficaTest extends Application {
 	public void start(Stage primaryStage) {
 		Graph<String> g1 = new Graph<String>();
 		
-		Node<String> a = new Node<String>("A", 100., 100.);
+		Node<String> a = new Node<String>("A");
 		Node<String> b = new Node<String>("B");
 		Node<String> c = new Node<String>("C");
 		Node<String> d = new Node<String>("D");
 		Node<String> e = new Node<String>("E");
-		Node<String> f = new Node<String>("F");
-		Node<String> g = new Node<String>("G");
-		Node<String> h = new Node<String>("H");
 		
 		
 		g1.insertNode(a);
@@ -30,18 +27,18 @@ public class GraficaTest extends Application {
 		g1.insertNode(c);
 		g1.insertNode(d);
 		g1.insertNode(e);
-		g1.insertNode(f);
-		g1.insertNode(g);
-		g1.insertNode(h);
 		
 		g1.insertEdge(a, b, 1);
 		g1.insertEdge(a, c, 2);
 		g1.insertEdge(b, c, 3);
+		g1.insertEdge(d, a, 4);
+		g1.insertEdge(d, e, 5);
+		g1.insertEdge(c, e, 6);
 		
 		Double Width = 1000.;
 		Double Height = 700.;
 		Pane console = new Pane();
-		console = g1.draw(40., Width, Height);
+		console = g1.draw(50., Width, Height);
 		Scene scene = new Scene(console);
 		primaryStage.setTitle("Welcome to FX!");
 		primaryStage.setHeight(Height);
